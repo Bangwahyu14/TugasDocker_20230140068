7acba14aa46337cc561bdc7c907dec189aae1188
Wahyu Agung Prasetyo <wahyu.agung.ft23@mail.umy.ac.id>
2026-04-13 11:18:38 +0700

    add User model with nama, nim, jenisKelamin, jurusan fields

--- src/main/java/com/example/pertemuan6/model/User.java
+++ src/main/java/com/example/pertemuan6/model/User.java
@@ -1,4 +1,12 @@
 package com.example.pertemuan6.model;
 
+import lombok.Data;
+import lombok.AllArgsConstructor;
+
+@Data
+@AllArgsConstructor
 public class User {
+    private String nama;
+    private String nim;
+    private String jenisKelamin;
 }
